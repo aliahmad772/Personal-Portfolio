8d956634fd804b280de89f4a24e4cc5bbcf82a25
aliahmad772 <alijansomoyo@gmail.com>
2025-07-11 22:39:04 +0500

    Again correct Footer issue.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,8 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
-import HeroMain from './components/hero/HeroMain'
-import Footer from './components/footer/footer'
-import HireME from './components/hireMe/HireME'
-import { Routes, Route } from 'react-router-dom';
+
 import NavbarMain from './components/navbar/NavbarMain'
 
 function App() {
